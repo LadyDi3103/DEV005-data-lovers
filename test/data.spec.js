f80862510537fe76bad98238240db099aa7b04c3
LadyDi3103 <chinamy2004@gmail.com>
2023-03-24 09:24:04 -0500

    testea las funciones

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,74 @@
-import { example, anotherExample } from '../src/data.js';
+// import { example, anotherExample } from '../src/data.js';
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
+
+//   it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
+
+
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
+
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
+
+import { sortData,filtrar,filtrar1, calcular } from '../src/data.js';
+
+const 
+
+
+
+
+
+describe('sortData', () => {
+  it('Debe ordenar de la (A-Z)', () => {
+    expect(typeof sortData).toBe('function');
+  });
+  it('Debe ordenar de la (Z-A)', () => {
+    expect(typeof sortData).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns `sortData`', () => {
+    expect(sortData()).toBe('function');
   });
 });
 
-
-describe('anotherExample', () => {
+describe('filtrar', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof filtrar).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns `filtrar`', () => {
+    expect(filtrar()).toBe('function');
+  });
+});
+
+describe('filtrar1', () => {
+  it('is a function', () => {
+    expect(typeof filtrar1).toBe('function');
+  });
+  
+  it('returns `filtrar1`', () => {
+    expect(filtrar1()).toBe('funtion');
+  });
+});
+
+describe('calcular', () => {
+  it('is a function', () => {
+    expect(typeof calcular).toBe('function');
+  });
+  
+  it('returns `calcular`', () => {
+    expect(calcular()).toBe('function');
   });
 });
